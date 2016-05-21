19a8fe28e9a8f8945406a91481d2dd955ed80f8f
fishwang2016 <fish@taifanmagellan.com>
2016-05-21 16:15:10 +0800

    added try work

--- js/app.js
+++ js/app.js
@@ -25,6 +25,28 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(){
+
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(dt){
+
+};
+Player.prototype.render = function(){
+
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+};
+
+Player.prototype.handleInput = function(){
+
+};
+
+
+var allEnemies =[new Enemy(),new Enemy()];
+
+var player = new Player();
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
